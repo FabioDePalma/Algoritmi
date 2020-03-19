0ee6679f32167313136e79da7c1e76c41ef8690d
Fabio De Palma <fabio.depalma@edu.unito.it>
2020-03-19 13:50:32 +0100

    gitignore file

--- src/C/Esercizio1.c
+++ src/C/Esercizio1.c
@@ -7,7 +7,7 @@ void swap(int *a, int *b){
 void insertionSort(int a[], int length){
   int i;
   int j;
-  for(i = 0; i<length; i++){
+  for(i = 1; i<length; i++){
     j = i;
     while(j > 0 && a[j] < a[j-1]){
       swap(&a[j], &a[j-1]);
@@ -16,15 +16,16 @@ void insertionSort(int a[], int length){
   }
 }
 
-void insertionSort(void* arr, ){
+//void insertionSort(void* arr, ){
 
-}
+//}
 
 int main(int argc, char const *argv[]) {
-  int a[] = {4, 6,2,5,7,9};
+  int a[] = {4,6,2,5,7,9};
   int i;
   int length = sizeof(a)/sizeof(int);
   for(i = 0; i<length; i++){
+
     printf( "%d ", a[i] );
   }
   printf("\n" );
@@ -33,4 +34,13 @@ int main(int argc, char const *argv[]) {
     printf( "%d ", a[i] );
   }
   return 0;
+  []field 1;
+  []field 2;
+  []field 3;
+  field1 = insertionSort(field1, lenghtfield1());
+  field2 = insertionSort(field2, lenghtfield2());
+  field3 = insertionSort(field3, lenghtfield23());
+  for(i = 0; i< lenghtfield1(); i++){
+    writefile(i + field1[i] + "," field2[i] + "," field3);
+  }
 }
